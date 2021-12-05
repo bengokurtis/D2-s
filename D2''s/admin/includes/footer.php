5378616c9bb6dae48503366767bad39d0aa39384
bengo <53141115+bengokurtis@users.noreply.github.com>
2021-12-05 08:15:37 +0300

    Add files via upload

---
+++ D2''s/admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+				D2' Hotelier <a href="#" target="_blank"><span>developed by </span> Blacc & Team</a>
+			</div>
---
+++ D2''s/heads/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+				D2's Management System <a href="#" target="_blank"><span>developed by </span> Blacc & Team</a>
+			</div>
